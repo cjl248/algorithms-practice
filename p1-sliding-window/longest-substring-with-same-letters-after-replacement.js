559618df7b1a65b7de36969ebad0275f0fc7a746
github cjl248 <github cjl248@cornell.edu>
2020-12-19 12:51:33 -0500

    Solved longest-substr-w-same-letters-after-repl and added the next algorithm file.

--- p1-sliding-window/longest-substring-with-same-letters-after-replacement.js
+++ p1-sliding-window/longest-substring-with-same-letters-after-replacement.js
@@ -1,48 +1,40 @@
 const length_of_longest_substring = function(str, k) {
-  // k = 1
-  // str = a b b c d c
 
-  // windowStart = 0
-  // windowEnd = 0
   // start walking through string with for loop
   // create a alphabet counter object
   // add each letter to an obj of counts
   // duplicate k as variable: replacement
-  // decrease replacement
-  //
-  let windowStart = 0;
-  let maxLength = 0;
-  let maxLetter;
-  const alphaCount = {};
+  // decrease replacement = (currentWindowLength - value of letter appearing most times)
 
+  let windowStart = 0
+  let maxLength = 0
+  let maxLetter
+  const alphaCount = {}
   for(let windowEnd = 0; windowEnd < str.length; windowEnd++) {
-    const currLetter = str[windowEnd];
+    const currLetter = str[windowEnd]
 
     if(alphaCount[currLetter]) {
-      alphaCount[currLetter]++;
+      alphaCount[currLetter]++
     } else {
-      alphaCount[currLetter] = 1;
+      alphaCount[currLetter] = 1
     }
-
-    let currSubLength = windowEnd - windowStart + 1;
-
-    let maxValue = 0;
+    let currSubLength = windowEnd - windowStart + 1
+    let maxValue = 0
     for(let letter in alphaCount) {
       if(alphaCount[letter] > maxValue) {
-        maxLetter = letter;
-        maxValue = alphaCount[letter];
+        maxLetter = letter
+        maxValue = alphaCount[letter]
       }
     }
-    // A A B B C
-    while(str[windowEnd] !== maxLetter && currSubLength - maxValue >= k) {
-      if(alphaCount[windowStart] === 1) {
-        delete alphaCount[windowStart];
-      } else {
-        alphaCount[windowStart]--;
-      }
-      windowStart++;
+    while(str[windowEnd] !== maxLetter && currSubLength - maxValue > k) {
+        alphaCount[windowStart]--
+        if(alphaCount[windowStart] === 1) {
+          delete alphaCount[windowStart]
+        }
+        windowStart++
+        currSubLength = windowEnd - windowStart + 1
     }
-    maxLength = Math.max(maxLength, currSubLength);
+    maxLength = Math.max(maxLength, currSubLength)
   }
-  return maxLength;
-};
+  return maxLength
+}
